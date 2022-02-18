302368e6fbe9578e3739989cfaa062ba237e482d
benjamin <benjamin@macbook-pro-de-benjamin.home>
2022-02-18 21:53:41 +0100

    end with mock server

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -4,35 +4,38 @@ import FeedbackItem from "./FeedbackItem"
 import {motion, AnimatePresence, MotionConfig} from 'framer-motion'
 import {useContext} from 'react'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 function FeedbackList(/*{ feedback, handleDelete }*/) {
     //Utilisation du Contexte
-    const {feedback} = useContext(FeedbackContext)
+    const { feedback, isLoading } = useContext(FeedbackContext)
 
-    if(!feedback || feedback.length === 0) {
+    if(!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No FeedBack yet</p>
     }
 
-      return (
-     <div className="feedback-list">
-         <AnimatePresence>
-         {feedback.map((item) => (
-             <motion.div //animation 
-                key={item.id}
-                initial={{opacity: 0}}
-                animate={{opacity: 1}}
-                exit={{opacity: 0}}
-             >
-                <FeedbackItem 
-                    key={item.id} 
-                    item={item} 
-                    // handleDelete={handleDelete}
-                />
-             </motion.div>
-         ))}
-         </AnimatePresence>
-     </div>
-  )
+    return isLoading ? <Spinner /> : ( <div className="feedback-list">
+    <AnimatePresence>
+    {feedback.map((item) => (
+        <motion.div //animation 
+           key={item.id}
+           initial={{opacity: 0}}
+           animate={{opacity: 1}}
+           exit={{opacity: 0}}
+        >
+           <FeedbackItem 
+               key={item.id} 
+               item={item} 
+               // handleDelete={handleDelete}
+           />
+        </motion.div>
+    ))}
+    </AnimatePresence>
+</div>)
+
+      
+    
+  
 }
 
 //Plus utilisé, car on utilise le Contexte à la place des props pour feedback
