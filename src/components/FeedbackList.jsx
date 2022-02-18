632893e64c867574b1f85cae06a5a29b27d89e4d
benjamin <benjamin@macbook-pro-de-benjamin.home>
2022-02-18 19:15:58 +0100

    update application

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,49 @@
+//import PropTypes from 'prop-types'
+import FeedbackItem from "./FeedbackItem"
+//npm i framer-motion@4.1.17
+import {motion, AnimatePresence, MotionConfig} from 'framer-motion'
+import {useContext} from 'react'
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackList(/*{ feedback, handleDelete }*/) {
+    //Utilisation du Contexte
+    const {feedback} = useContext(FeedbackContext)
+
+    if(!feedback || feedback.length === 0) {
+        return <p>No FeedBack yet</p>
+    }
+
+      return (
+     <div className="feedback-list">
+         <AnimatePresence>
+         {feedback.map((item) => (
+             <motion.div //animation 
+                key={item.id}
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                exit={{opacity: 0}}
+             >
+                <FeedbackItem 
+                    key={item.id} 
+                    item={item} 
+                    // handleDelete={handleDelete}
+                />
+             </motion.div>
+         ))}
+         </AnimatePresence>
+     </div>
+  )
+}
+
+//Plus utilisé, car on utilise le Contexte à la place des props pour feedback
+// FeedbackList.propTypes = {
+//     feedback: PropTypes.arrayOf(
+//         PropTypes.shape({
+//             id: PropTypes.number.isRequired,
+//             text: PropTypes.string.isRequired,
+//             rating: PropTypes.number.isRequired,
+//         })
+//     )
+// }
+
+export default FeedbackList
